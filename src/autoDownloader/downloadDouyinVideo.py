309508de4b8fce2de6adecaa75e12562cd632b0b
huobaolajiao <38094576+huobaolajiao@users.noreply.github.com>
2020-01-23 12:54:58 +0800

    change douyin to local downloaded video source

--- src/autoDownloader/downloadDouyinVideo.py
+++ src/autoDownloader/downloadDouyinVideo.py
@@ -12,24 +12,26 @@ import threading
 import os
 import database.dbConn as dbConn
 import log.logCenter as logCenter
-
-
+from urllib.parse import quote
 
 def downloadOneVideo(name,title,url,timestamp):
     conn=dbConn.getConn()
     global VideoCount
     try:
-        os.chdir(os.path.join(os.path.abspath('..'),'autoDownloader'))
+        os.chdir(os.path.join(os.path.abspath('..'),'web','static'))
         head = {'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'}
         page = requests.get(url.replace("/playwm/","/play/"), headers=head)
         page.encoding = 'utf-8'
         #print(page.url)
         webvideopage = requests.get(page.url,headers=head)
-        if(os.path.exists(os.path.join("download",name))==0):
-            os.makedirs(os.path.join("download",name))
-        with open(os.path.join("download",name,title+"_"+str(timestamp)+".mp4"), "wb") as code:
+        if(os.path.exists(os.path.join("video",name))==0):
+            os.makedirs(os.path.join("video",name))
+        with open(os.path.join("video",name,title+"_"+str(timestamp)+".mp4"), "wb") as code:
             code.write(webvideopage.content)
-        conn.execute("UPDATE rssData set downloaded = 1  where rssName='{}' and title='{}'".format(name,title))
+        summary='''<p style="display:block; text-align:center;">
+        <video controls="controls" width="300" height="600" src="/video/{}/{}">
+        </video></p>'''.format(name,quote(title)+"_"+str(timestamp)+".mp4")
+        conn.execute("UPDATE rssData set downloaded = 1,summary='{}'  where rssName='{}' and title='{}'".format(summary,name,title))
         conn.commit()
         VideoCount=VideoCount+1
         logger.info(name+" "+url+" downloaded")
